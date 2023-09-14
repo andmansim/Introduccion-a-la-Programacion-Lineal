30bbeb8d1c9acaf8957b22c0567e0c02cdbaa86f
andmansim <91722324+andmansim@users.noreply.github.com>
2023-09-14 18:01:53 +0200

    Update Codigo.py
    Variables a optimizar

--- Codigo.py
+++ Codigo.py
@@ -1,1 +1,4 @@
-
+#variables
+espadachines = solver.IntVar(0, solver.infinity(),"espadachines")
+jinetes = solver.IntVar(0, solver.infinity(),jinetes")
+arqueros = solver.IntVar(0, solver.infinity(),"arqueros")                        
