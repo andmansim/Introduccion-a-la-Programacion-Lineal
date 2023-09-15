eab917cc7fd568090be0ee4ce5ef9fcc2ce35937
andmansim <91722324+andmansim@users.noreply.github.com>
2023-09-15 12:15:11 +0200

    optimizamos el código y presentamos la solucion

--- Codigo.py
+++ Codigo.py
@@ -5,15 +5,26 @@ from ortools.linear_solver import pywraplp
 solucionador = pywraplp.Solver("Maximizar la fuerza de los guerreros", pywraplp.Solver.GLOP_LINEAR_PROGRAMMING)
 
 #variables a optimizar
-espadachines = solver.IntVar(0, solver.infinity(),"espadachines")
-jinetes = solver.IntVar(0, solver.infinity(),"jinetes")
-arqueros = solver.IntVar(0, solver.infinity(),"arqueros")                        
+espadachines = solucionador .IntVar(0, solucionador .infinity(),"espadachines")
+jinetes = solucionador .IntVar(0, solucionador .infinity(),"jinetes")
+arqueros = solucionador .IntVar(0, solucionador .infinity(),"arqueros")                        
 
 #Restricciones de las variables
-solver.Add(60*espadachines + 80* arqueros + 140* jinetes <= 1200)
-solver.Add(20*espadachines + 10* arqueros + 0* jinetes <= 800)
-solver.Add(0*espadachines + 40* arqueros + 100* jinetes <= 600)
+solucionador .Add(60*espadachines + 80* arqueros + 140* jinetes <= 1200)
+solucionador .Add(20*espadachines + 10* arqueros + 0* jinetes <= 800)
+solucionador .Add(0*espadachines + 40* arqueros + 100* jinetes <= 600)
 
 #Función para maximizar
-solver.Maximize(70*espadachines + 95*arqueros + 230*jinetes)
+solucionador .Maximize(70*espadachines + 95*arqueros + 230*jinetes)
+
+#Optimizamos el código
+estado = solucionador.Solve()
+
+if estado == pywraplp.Solver.OPTIMAL:
+    print("********************************************************")
+    print(f'Resuelto en {solucionador.wall_time():.2f}milisegundos en {solucionador.iterations()}iteraciones')
+    print(f'Hemos optenido {solucionador.Objective().Value()} poder')
+    print(f'Espadachines = {espadachines.solution_value()}')
+    print(f'Arqueros = {arqueros.solution_value()}')
+    print(f'Jinetes = {jinetes.solution_value()}')
 
